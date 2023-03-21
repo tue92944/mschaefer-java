3b352e27035953e0acf4a6f50c59b0b9a9922c25
Meghan Schaefer <88904519+tue92944@users.noreply.github.com>
2023-03-21 14:49:33 -0400

    added isHealthy boolean

--- M1/exercise2/src/Main.java
+++ M1/exercise2/src/Main.java
@@ -8,14 +8,18 @@ public class Main {
         int age = 32;
         int height = 2;
         int weight = 63;
+        boolean isHealthy;
         float bmi = weight/(height*height);
         if (bmi >= bmiLowThreshold && bmi <= bmiHighThreshold){
+            isHealthy=true;
             System.out.println("Good Job!");
         } else if (bmi< bmiLowThreshold) {
+            isHealthy=false;
             System.out.println("Here's a snack");
 
         }
         else{
+            isHealthy=false;
             System.out.println("do some more work");
         }
 
