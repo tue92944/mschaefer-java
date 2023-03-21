cf203e3d210e4159f2dee0f15b95b75c7b95fc91
Meghan Schaefer <88904519+tue92944@users.noreply.github.com>
2023-03-21 15:48:53 -0400

    added exercise 3

---
+++ M1/exercise3/src/PaystubCalculator.java
@@ -0,0 +1,26 @@
+public class PaystubCalculator {
+    public static void main(String[] args) {
+        int totalHoursWorked, overTimeHours;
+        double hourlyRate, overTimeRate, grossPay;
+        boolean workedOvertime;
+
+        hourlyRate= 40.0;
+        overTimeRate= hourlyRate * 1.5;
+        totalHoursWorked = 45;
+
+
+        workedOvertime= totalHoursWorked>40;
+
+        if(!workedOvertime){
+            grossPay= hourlyRate * totalHoursWorked;
+            System.out.println("Your gross pay is " + grossPay);
+        }
+        else {
+            overTimeHours= totalHoursWorked-40;
+            grossPay= (hourlyRate * (totalHoursWorked- overTimeHours)) + (overTimeRate * overTimeHours);
+            System.out.println("Your gross pay with overtime is " + grossPay);
+        }
+
+
+    }
+}
