24de161acce3a570ad5f5f9239cd94753daa3f42
Meghan Schaefer <88904519+tue92944@users.noreply.github.com>
2023-03-23 12:55:27 -0400

    added decisions to shoppingcart

--- M1/exercise-shopping-cart-start-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
+++ M1/exercise-shopping-cart-start-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
@@ -10,43 +10,17 @@ public class ShoppingCartApp {
         int productCategory = 2;
         float productCost = 2.56f;
         float productPrice = 4.99f;
-        int productQuantity = 78;
 
-
-//        float totalCost = productQuantity * productCost;
-//        System.out.println("Total Cost is $" + totalCost);
-//
-//
-//        float profitMargin = (productPrice - productCost);
-//        System.out.printf("Profit Margin = $%.2f", profitMargin);
-//
-//        float potentialProfit = (profitMargin * productQuantity);
-//        System.out.println("\nPotential profit is $" + potentialProfit);
-//
-//        //declare all values of enum in an array
-//        OrderStatus[] orderStatus = OrderStatus.values();
-//        ShippingStatus[] shippingStatus = ShippingStatus.values();
-//
-//        //print out enum values after converting to a string
-//        System.out.println(Arrays.toString(orderStatus));
-//        System.out.println(Arrays.toString(shippingStatus));
-//
-//        //STRING EXERCISE
-//        String businessName = "Shopping Mania";
-//        String contactInfo = "909-123-5678";
-//        String itemDescription = "Fresh Bananas";
-//
-//        System.out.println(businessName);
-//        System.out.println(contactInfo);
-//        System.out.println(itemDescription);
-
-        //CONSOLE IO EXERCISE
+        //get user input for shipping and quantity
         Scanner sc = new Scanner(System.in);
         System.out.println("Are you tax-exempt?Y/N");
         String taxExempt = sc.nextLine();
 
         System.out.println("Shipping? Y/N");
         String shipping = sc.nextLine();
+
+        System.out.println("What type of shipping? Standard/TwoDay/Overnight");
+        String shippingType = sc.nextLine().toLowerCase();
 
         System.out.println("Order quantity?");
         String quantityStr = sc.nextLine();
@@ -57,6 +31,90 @@ public class ShoppingCartApp {
 
         System.out.println("Details:");
         System.out.printf("Tax-Exempt? %s\nShipping? %s\nOrder Quantity: %s\nPromo Code: %s\n", taxExempt, shipping, quantity, promoCode);
+
+
+        float profitMargin = (productPrice - productCost);
+        float potentialProfit = (profitMargin * quantity);
+        float taxRate = .07f;
+        float standardShipping = 2.00f;
+        float twoDayShipping = 5.00f;
+        float overnightShipping = 10.00f;
+        float totalCost = (quantity * productCost)* taxRate;
+        float totalCostTaxExempt = quantity * productCost;
+
+        //declare all values of enum in an array
+        OrderStatus[] orderStatus = OrderStatus.values();
+        ShippingStatus[] shippingStatus = ShippingStatus.values();
+
+        //print out enum values after converting to a string
+        System.out.println(Arrays.toString(orderStatus));
+        System.out.println(Arrays.toString(shippingStatus));
+
+        if (taxExempt.equals("Y")){
+            if (shipping.equals("N")){
+                System.out.printf("Your total cost without tax or shipping is: %s", totalCostTaxExempt);
+            }else{
+                switch(shippingType) {
+                    case "standard": {
+                        totalCostTaxExempt += standardShipping;
+                        System.out.printf("Your total cost without tax and standard shipping is: %s", totalCostTaxExempt);
+                        break;
+                    }
+                    case "twoday": {
+                        totalCostTaxExempt += twoDayShipping;
+                        System.out.printf("Your total cost without tax and two-day shipping is: %s", totalCostTaxExempt);
+                        break;
+                    }
+                    case "overnight": {
+                        totalCostTaxExempt += overnightShipping;
+                        System.out.printf("Your total cost without tax and overnight shipping is: %s", totalCostTaxExempt);
+                        break;
+                    }
+                }
+            }
+        } else{
+            if (shipping.equals("N")){
+                System.out.printf("Your total cost with tax and no shipping is: %s", totalCost);
+            }else{
+                switch(shippingType) {
+                    case "standard": {
+                        totalCost += standardShipping;
+                        System.out.printf("Your total cost without tax and standard shipping is: %s", totalCost);
+                        break;
+                    }
+                    case "twoday": {
+                        totalCost += twoDayShipping;
+                        System.out.printf("Your total cost without tax and two-day shipping is: %s", totalCost);
+                        break;
+                    }
+                    case "overnight": {
+                        totalCost += overnightShipping;
+                        System.out.printf("Your total cost without tax and overnight shipping is: %s", totalCost);
+                        break;
+                    }
+                }
+            }
+
+
+
+
+
+        }
+
+        //STRING EXERCISE
+//        String businessName = "Shopping Mania";
+//        String contactInfo = "909-123-5678";
+//        String itemDescription = "Fresh Bananas";
+//
+//        System.out.println(businessName);
+//        System.out.println(contactInfo);
+//        System.out.println(itemDescription);
+
+        //DECISIONS EXERCISE
+
+
+
+
 
 
 
