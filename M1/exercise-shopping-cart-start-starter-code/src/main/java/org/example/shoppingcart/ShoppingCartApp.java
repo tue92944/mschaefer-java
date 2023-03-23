4eb22ddec3ee776c7e41c13d48a01191a5ac9aea
Meghan Schaefer <88904519+tue92944@users.noreply.github.com>
2023-03-23 14:39:19 -0400

    added promo code to shopping cart exercise

--- M1/exercise-shopping-cart-start-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
+++ M1/exercise-shopping-cart-start-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
@@ -41,58 +41,205 @@ public class ShoppingCartApp {
         float overnightShipping = 10.00f;
         float totalCost = (quantity * productCost)* taxRate;
         float totalCostTaxExempt = quantity * productCost;
+        float fiveHundredDiscountWithTax = totalCost * .1f;
+        float fiveHundredDiscountWithoutTax = totalCostTaxExempt * .1f;
+        float oneHundredDiscountWithTax = totalCost * .05f;
+        float oneHundredDiscountWithoutTax = totalCostTaxExempt * .05f;
 
         //declare all values of enum in an array
-        OrderStatus[] orderStatus = OrderStatus.values();
-        ShippingStatus[] shippingStatus = ShippingStatus.values();
-
-        //print out enum values after converting to a string
-        System.out.println(Arrays.toString(orderStatus));
-        System.out.println(Arrays.toString(shippingStatus));
+//        OrderStatus[] orderStatus = OrderStatus.values();
+//        ShippingStatus[] shippingStatus = ShippingStatus.values();
+//
+//        //print out enum values after converting to a string
+//        System.out.println(Arrays.toString(orderStatus));
+//        System.out.println(Arrays.toString(shippingStatus));
 
         if (taxExempt.equals("Y")){
             if (shipping.equals("N")){
-                System.out.printf("Your total cost without tax or shipping is: %s", totalCostTaxExempt);
-            }else{
-                switch(shippingType) {
-                    case "standard": {
-                        totalCostTaxExempt += standardShipping;
-                        System.out.printf("Your total cost without tax and standard shipping is: %s", totalCostTaxExempt);
-                        break;
-                    }
-                    case "twoday": {
-                        totalCostTaxExempt += twoDayShipping;
-                        System.out.printf("Your total cost without tax and two-day shipping is: %s", totalCostTaxExempt);
-                        break;
-                    }
-                    case "overnight": {
-                        totalCostTaxExempt += overnightShipping;
-                        System.out.printf("Your total cost without tax and overnight shipping is: %s", totalCostTaxExempt);
-                        break;
-                    }
+                if (totalCostTaxExempt > 500){
+                    totalCostTaxExempt -= fiveHundredDiscountWithoutTax;
+                    System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                    System.out.printf("Your total cost without tax or shipping is: %.2f\n", totalCostTaxExempt);
                 }
+                else if (totalCostTaxExempt > 100){
+                    totalCostTaxExempt -= oneHundredDiscountWithoutTax;
+                    System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                    System.out.printf("Your total cost without tax or shipping is: %.2f\n", totalCostTaxExempt);
+                }
+                else{
+                    System.out.printf("Your total cost without tax or shipping is: %.2f\n", totalCostTaxExempt);
+                }
+
+            }else{
+                if (totalCostTaxExempt > 500){
+                    switch(shippingType) {
+                        case "standard": {
+                            if (promoCode.equals("free")){
+                                totalCostTaxExempt -= fiveHundredDiscountWithoutTax;
+                                System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                                System.out.printf("Your total cost without tax and free shipping is: %.2f\n", totalCostTaxExempt);
+                                break;
+                            }else{
+                                totalCostTaxExempt -= fiveHundredDiscountWithoutTax;
+                                totalCostTaxExempt += standardShipping;
+                                System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                                System.out.printf("Your total cost without tax and standard shipping is: %.2f\n", totalCostTaxExempt);
+                            }
+
+                        }
+                        case "twoday": {
+                            totalCostTaxExempt -= fiveHundredDiscountWithoutTax;
+                            totalCostTaxExempt += twoDayShipping;
+                            System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                            System.out.printf("Your total cost without tax and two-day shipping is: %.2f\n", totalCostTaxExempt);
+                            break;
+                        }
+                        case "overnight": {
+                            totalCostTaxExempt -= fiveHundredDiscountWithoutTax;
+                            totalCostTaxExempt += overnightShipping;
+                            System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                            System.out.printf("Your total cost without tax and overnight shipping is: %.2f\n", totalCostTaxExempt);
+                            break;
+                        }
+                    }
+                }else if (totalCostTaxExempt > 100){
+                    switch(shippingType) {
+                        case "standard": {
+                            if (promoCode.equals("free")){
+                                totalCostTaxExempt -= oneHundredDiscountWithoutTax;
+                                System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                                System.out.printf("Your total cost without tax and free shipping is: %.2f\n", totalCostTaxExempt);
+                                break;
+                            }else{
+                                totalCostTaxExempt -= oneHundredDiscountWithoutTax;
+                                totalCostTaxExempt += standardShipping;
+                                System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                                System.out.printf("Your total cost without tax and standard shipping is: %.2f\n", totalCostTaxExempt);
+                                break;
+                            }
+                        }
+                        case "twoday": {
+                            totalCostTaxExempt -= oneHundredDiscountWithoutTax;
+                            totalCostTaxExempt += twoDayShipping;
+                            System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                            System.out.printf("Your total cost without tax and two-day shipping is: %.2f\n", totalCostTaxExempt);
+                            break;
+                        }
+                        case "overnight": {
+                            totalCostTaxExempt -= oneHundredDiscountWithoutTax;
+                            totalCostTaxExempt += overnightShipping;
+                            System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                            System.out.printf("Your total cost without tax and overnight shipping is: %.2f\n", totalCostTaxExempt);
+                            break;
+                        }
+                    }
+                }else{
+                    switch(shippingType) {
+                        case "standard": {
+                            totalCostTaxExempt += standardShipping;
+                            System.out.printf("Your total cost without tax and standard shipping is: %.2f\n", totalCostTaxExempt);
+                            break;
+                        }
+                        case "twoday": {
+                            totalCostTaxExempt += twoDayShipping;
+                            System.out.printf("Your total cost without tax and two-day shipping is: %.2f\n", totalCostTaxExempt);
+                            break;
+                        }
+                        case "overnight": {
+                            totalCostTaxExempt += overnightShipping;
+                            System.out.printf("Your total cost without tax and overnight shipping is: %.2f\n", totalCostTaxExempt);
+                            break;
+                        }
+                    }
+
+                }
+
             }
         } else{
             if (shipping.equals("N")){
-                System.out.printf("Your total cost with tax and no shipping is: %s", totalCost);
+                if (totalCost > 500){
+                    totalCost -= fiveHundredDiscountWithTax;
+                    System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                    System.out.printf("Your total cost with tax and no shipping is: %.2f\n", totalCost);
+                }
+                else if (totalCostTaxExempt > 100){
+                    totalCost -= oneHundredDiscountWithTax;
+                    System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                    System.out.printf("Your total cost with tax or shipping is: %.2f\n", totalCost);
+                }
+                else{
+                    System.out.printf("Your total cost with tax and no shipping is: %.2f\n", totalCost);
+                }
             }else{
-                switch(shippingType) {
-                    case "standard": {
-                        totalCost += standardShipping;
-                        System.out.printf("Your total cost with tax and standard shipping is: %s", totalCost);
-                        break;
+                if (totalCost > 500){
+                    switch(shippingType) {
+                        case "standard": {
+                            totalCost -= fiveHundredDiscountWithTax;
+                            totalCost += standardShipping;
+                            System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                            System.out.printf("Your total cost with tax and standard shipping is: %.2f\n", totalCost);
+                            break;
+                        }
+                        case "twoday": {
+                            totalCost -= fiveHundredDiscountWithTax;
+                            totalCost += twoDayShipping;
+                            System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                            System.out.printf("Your total cost with tax and two-day shipping is: %.2f\n", totalCost);
+                            break;
+                        }
+                        case "overnight": {
+                            totalCost -= fiveHundredDiscountWithTax;
+                            totalCost += overnightShipping;
+                            System.out.println("Congrats! Since your order was over $500 we gave you a 10% discount.");
+                            System.out.printf("Your total cost with tax and overnight shipping is: %.2f\n", totalCost);
+                            break;
+                        }
                     }
-                    case "twoday": {
-                        totalCost += twoDayShipping;
-                        System.out.printf("Your total cost with tax and two-day shipping is: %s", totalCost);
-                        break;
+                }else if (totalCost > 100){
+                    switch(shippingType) {
+                        case "standard": {
+                            totalCost -= oneHundredDiscountWithTax;
+                            totalCost += standardShipping;
+                            System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                            System.out.printf("Your total cost with tax and standard shipping is: %.2f\n", totalCost);
+                            break;
+                        }
+                        case "twoday": {
+                            totalCost -= oneHundredDiscountWithTax;
+                            totalCost += twoDayShipping;
+                            System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                            System.out.printf("Your total cost with tax and two-day shipping is: %.2f\n", totalCost);
+                            break;
+                        }
+                        case "overnight": {
+                            totalCost -= oneHundredDiscountWithTax;
+                            totalCost += overnightShipping;
+                            System.out.println("Congrats! Since your order was over $100 we gave you a 5% discount.");
+                            System.out.printf("Your total cost with tax and overnight shipping is: %.2f\n", totalCost);
+                            break;
+                        }
                     }
-                    case "overnight": {
-                        totalCost += overnightShipping;
-                        System.out.printf("Your total cost with tax and overnight shipping is: %s", totalCost);
-                        break;
+
+                }else{
+                    switch(shippingType) {
+                        case "standard": {
+                            totalCost += standardShipping;
+                            System.out.printf("Your total cost with tax and standard shipping is: %.2f\n", totalCost);
+                            break;
+                        }
+                        case "twoday": {
+                            totalCost += twoDayShipping;
+                            System.out.printf("Your total cost with tax and two-day shipping is: %.2f\n", totalCost);
+                            break;
+                        }
+                        case "overnight": {
+                            totalCost += overnightShipping;
+                            System.out.printf("Your total cost with tax and overnight shipping is: %.2f\n", totalCost);
+                            break;
+                        }
                     }
                 }
+
             }
 
 
