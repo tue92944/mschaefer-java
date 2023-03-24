189dea341ad32eb917e8b46c6cb1dd7ae199f3f8
Meghan Schaefer <88904519+tue92944@users.noreply.github.com>
2023-03-24 11:03:14 -0400

    updated warmup

--- M1/warmups/warmup_032423/src/Main.java
+++ M1/warmups/warmup_032423/src/Main.java
@@ -1,36 +1,34 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String[] myArray = new String [4];
+        String[] names = new String [2];
         Scanner sc = new Scanner(System.in);
-        String empty = "";
+        String name = "";
+        int index = 0;
 
-
-        for (int i=0; i<myArray.length; i++){
-            System.out.println("Please enter your word: ");
-            String input = sc.nextLine();
-            if (input != empty){
-                myArray[i]=input;
-                System.out.println(myArray);
-            }else{
+        do{
+            System.out.println(("Enter a name"));
+            name = sc.nextLine();
+            if (name.isBlank()){
                 break;
             }
-
-        }
-
-        String[] doubleMyArray = new String [myArray.length * 2];
-
-        for (int i = myArray.length; i<doubleMyArray.length;i++){
-            doubleMyArray[i]= myArray[i];
-            System.out.println("Please enter your word: ");
-            String input = sc.nextLine();
-            if (input != empty){
-                doubleMyArray[i]=input;
-                System.out.println(doubleMyArray);
-            }else{
-                break;
+            names[index] = name;
+            index++;
+            if (index == names.length){
+                String[] newArray = new String[names.length *2];
+                for (int i =0; i<names.length; i++){
+                    newArray[i] = names[i];
+                }
+                names = newArray;
             }
-        }
+
+        }while(name.isBlank() == false);
+
+
+
+
+
+
 
 
 
