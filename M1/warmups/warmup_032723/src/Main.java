760c721c963a192fcb680d2ca374884d430cb8ae
Meghan Schaefer <88904519+tue92944@users.noreply.github.com>
2023-03-27 10:22:31 -0400

    added warmup for 032723

---
+++ M1/warmups/warmup_032723/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static int sumArray(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 3 == 0) {
+                sum += numbers[i];
+            }
+
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int[] numbers = {3,6,9};
+        System.out.println(sumArray(numbers));
+
+        }
+    }
+
+
+
