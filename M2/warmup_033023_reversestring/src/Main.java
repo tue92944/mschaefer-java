95597128efad57c69b265fa3492ea23014eda82d
Meghan Schaefer <88904519+tue92944@users.noreply.github.com>
2023-03-30 10:17:34 -0400

    added warmup for 033023

---
+++ M2/warmup_033023_reversestring/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static String reverseSentence (){
+        String sentence = getString("what is your input");
+        String[] sentenceArray = sentence.split(" ");
+        String result = "";
+        for (int i = sentenceArray.length-1; i>=0; i--){
+            result+= sentenceArray[i] + " ";
+        }
+        return result;
+
+
+    }
+
+    public static String getString(String prompt){
+        Scanner console = new Scanner(System.in);
+        System.out.println(prompt);
+        return console.nextLine();
+    }
+    public static void main(String[] args) {
+        System.out.println(reverseSentence());
+
+
+    }
+}
